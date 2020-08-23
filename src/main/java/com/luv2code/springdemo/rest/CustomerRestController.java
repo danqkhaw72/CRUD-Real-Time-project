d2b14bd2093d25aef956a3dabce5adb2e49babca
Dang Kham <danqkhaw72@gmail.com>
2020-08-23 16:49:15 +0700

    Delete customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,17 @@ public class CustomerRestController {
 		customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
+	}
+	
+	// Add mapping for DELETE /customer - delete  a customer
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Delete customer id - " + customerId;
+		
 	}
 	
 }
